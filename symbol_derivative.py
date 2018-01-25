8134448a4647afaacb8a3054891aa1b907df0643
tkda-h3 <3791992498@edu.k.u-tokyo.ac.jp>
2018-01-25 10:06:01 +0900

    tm p

--- symbol_derivative.py
+++ symbol_derivative.py
@@ -8,6 +8,7 @@ from interval import (
     imath,
 )
 from matplotlib.patches import Rectangle
+from matplotlib import cm
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
